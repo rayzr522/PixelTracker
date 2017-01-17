26b9ceaa8e55f7797c39848cac73d4a18df479b5
Rayzr522 <rayzr522@gmail.com>
2017-01-17 10:15:03 -0500

    Apparently 204 doesn't work right...

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,13 @@
+var path = require('path');
 var express = require('express');
 var app = express();
-var path = require('path');
 
 var pageViews = {};
 var ipViews = {};
 
 app.use('/', express.static(path.resolve(__dirname, '../client')));
 
-app.get('/pixel.gif', function (req, res) {
+app.get('/pixel.gif', function(req, res) {
     if (req.query && req.query.page) {
         if (!pageViews[req.query.page]) pageViews[req.query.page] = 0;
         pageViews[req.query.page] += 1;
@@ -21,13 +21,16 @@ app.get('/pixel.gif', function (req, res) {
     if (ip && ip.startsWith("::ffff:")) {
         ip = ip.replace("::ffff:", "")
     }
+    if (ip === '::1') {
+        ip = '127.0.0.1';
+    }
 
     if (ip) {
         if (!ipViews[ip]) ipViews[ip] = 0;
         ipViews[ip] += 1;
     }
 
-    res.status(204);
+    res.sendFile(path.resolve(__dirname, 'pixel.gif'));
 });
 
 app.get('/stats', (req, res) => {
